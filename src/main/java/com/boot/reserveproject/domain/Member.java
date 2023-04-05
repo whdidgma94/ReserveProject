f5a6eb1925d1c428090fdca8ae737997c0f0c0b1
JunyoungYoon <junyoungha2@naver.com>
2023-04-05 12:54:12 +0900

    .
    .

--- src/main/java/com/boot/reserveproject/domain/Member.java
+++ src/main/java/com/boot/reserveproject/domain/Member.java
@@ -1,4 +1,14 @@
 package com.boot.reserveproject.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Entity
 public class Member {
+
 }
