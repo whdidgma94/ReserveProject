8529d3dc292dce0d69e6569b6eb29d0c43d92432
JunyoungYoon <junyoungha2@naver.com>
2023-04-05 13:18:01 +0900

    ,
    ,

--- src/main/java/com/boot/reserveproject/domain/Camp.java
+++ src/main/java/com/boot/reserveproject/domain/Camp.java
@@ -1,4 +1,17 @@
 package com.boot.reserveproject.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "camp")
 public class Camp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "campId")
+    private Long id;
 }
--- src/main/java/com/boot/reserveproject/domain/Reservation.java
+++ src/main/java/com/boot/reserveproject/domain/Reservation.java
@@ -1,9 +1,20 @@
 package com.boot.reserveproject.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Reservation")
 public class Reservation {
-    int no;
-    String name ="junyoung123";
-    String name1 = "hi";
-    int num = 1;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservationNo")
+    private Long no;
+
+
 }
 
