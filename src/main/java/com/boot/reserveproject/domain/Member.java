b088fd7f32fbb91737952cd07e7e1d439eb9706b
JunyoungYoon <junyoungha2@naver.com>
2023-04-05 13:03:10 +0900

    ,
    ,

--- src/main/java/com/boot/reserveproject/domain/Member.java
+++ src/main/java/com/boot/reserveproject/domain/Member.java
@@ -4,11 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "member")
 public class Member {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "memberId")
+    private Long id;
+    @NotBlank
+    private String name;
+    private String Address;
 }
