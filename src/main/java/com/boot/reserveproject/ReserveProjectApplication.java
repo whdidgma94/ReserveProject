094cb8fa13e725e4787616342b30f522b9dff47d
ParkIlKwon <junyoungha2@naver.com>
2023-04-05 10:32:07 +0900

    Initial commit

---
+++ src/main/java/com/boot/reserveproject/ReserveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.boot.reserveproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/reserveproject/ReserveProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.reserveproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
