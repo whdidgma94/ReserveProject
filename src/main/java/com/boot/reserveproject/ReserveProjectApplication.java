4162972ec251d151c96aef8245becde2869219ec
ParkIlKwon <junyoungha2@naver.com>
2023-04-05 10:33:48 +0900

    m

--- src/main/java/com/boot/reserveproject/ReserveProjectApplication.java
+++ src/main/java/com/boot/reserveproject/ReserveProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReserveProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReserveProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveProjectApplication.class, args);
+    }
 
 }
