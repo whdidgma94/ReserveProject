c992d5d92c630072e392e8989576c81c49f3aa10
whdidgma94 <whdidgma94@naver.com>
2023-04-06 17:35:32 +0900

    add footer contents

--- src/main/resources/static/js/side-bar.js
+++ src/main/resources/static/js/side-bar.js
@@ -3,11 +3,12 @@ function handleGlobalClick(event) {
     const sideBar = document.querySelector('.side-bar');
     const header = document.querySelector('.header');
     const view = document.querySelector('.view');
-
+    const footer = document.querySelector(".footer");
     if (!sideBar.contains(event.target)) {
         sideBar.classList.remove('active');
         header.classList.remove('blur');
         view.classList.remove('blur');
+        footer.classList.remove('blur');
         sideBarCheck = false;
 
         document.removeEventListener('click', handleGlobalClick);
@@ -18,10 +19,12 @@ function viewSideBar() {
     const sideBar = document.querySelector('.side-bar');
     const header = document.querySelector('.header');
     const view = document.querySelector('.view');
+    const footer = document.querySelector(".footer");
 
     sideBar.classList.add('active');
     header.classList.add('blur');
     view.classList.add('blur');
+    footer.classList.add('blur');
     sideBarCheck = true;
 
     setTimeout(() => {
