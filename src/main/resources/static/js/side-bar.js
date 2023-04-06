f9323273b6a1e0b09966dc6b8151d519514d9ee1
whdidgma94 <whdidgma94@naver.com>
2023-04-06 14:20:30 +0900

    add sidebar close function

--- src/main/resources/static/js/side-bar.js
+++ src/main/resources/static/js/side-bar.js
@@ -1,5 +1,30 @@
+let sideBarCheck = false;
+function handleGlobalClick(event) {
+    const sideBar = document.querySelector('.side-bar');
+    const header = document.querySelector('.header');
+    const view = document.querySelector('.view');
 
-function viewSideBar(){
-    const sideBar = document.querySelector(".side-bar");
-    sideBar.className += " active";
+    if (!sideBar.contains(event.target)) {
+        sideBar.classList.remove('active');
+        header.classList.remove('blur');
+        view.classList.remove('blur');
+        sideBarCheck = false;
+
+        document.removeEventListener('click', handleGlobalClick);
+    }
+}
+
+function viewSideBar() {
+    const sideBar = document.querySelector('.side-bar');
+    const header = document.querySelector('.header');
+    const view = document.querySelector('.view');
+
+    sideBar.classList.add('active');
+    header.classList.add('blur');
+    view.classList.add('blur');
+    sideBarCheck = true;
+
+    setTimeout(() => {
+        document.addEventListener('click', handleGlobalClick);
+    }, 0);
 }
