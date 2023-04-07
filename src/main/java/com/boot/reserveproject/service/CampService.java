f27bba4420f300516eb1c987999b15449dfbf2f8
JunyoungYoon <junyoungha2@naver.com>
2023-04-07 18:48:09 +0900

    ,
    ,

--- src/main/java/com/boot/reserveproject/service/CampService.java
+++ src/main/java/com/boot/reserveproject/service/CampService.java
@@ -5,13 +5,34 @@ import com.boot.reserveproject.repository.CampRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CampService {
+    private final CampRepository campRepository;
     @Autowired
-    private CampRepository campRepository;
+    public CampService(CampRepository campRepository) {
+        this.campRepository = campRepository;
+    }
 
+    public void getAllCamps() {
+        List<Camp> campList = campRepository.findAll();
+
+
+    }
+    public List<Camp> getCampListByName(String keyword){
+        List<Camp> campList = campRepository.findByfacltNmContaining(keyword);
+        return campList;
+    }
+    public List<Camp> getCampListByTheme(String keyword){
+        List<Camp> campList = campRepository.findBythemaEnvrnClContaining(keyword);
+        return campList;
+    }
+    public List<Camp> getCampListByAddress(String keyword){
+        List<Camp> campList = campRepository.findByaddr1Containing(keyword);
+        return campList;
+    }
     public Camp getCampById(Long contentId) {
         Optional<Camp> camp = campRepository.findById(contentId);
         return camp.orElse(null);
