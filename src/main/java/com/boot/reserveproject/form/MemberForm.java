4722fe0806764b025de42133783e91331373132f
junyoungYoon <junyoungha2@naver.com>
2023-04-09 14:54:49 +0900

    m
    m

--- src/main/java/com/boot/reserveproject/form/MemberForm.java
+++ src/main/java/com/boot/reserveproject/form/MemberForm.java
@@ -1,9 +1,8 @@
-package com.boot.reserveproject.controller;
+package com.boot.reserveproject.form;
 
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Getter
