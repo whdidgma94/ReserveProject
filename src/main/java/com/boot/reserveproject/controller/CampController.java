0806ddb71390c84d96e401b2663c6cc45a3f59af
kimyounggyoun <50776744+kimyounggyoun@users.noreply.github.com>
2023-04-09 14:54:58 +0900

    asd

--- src/main/java/com/boot/reserveproject/controller/CampController.java
+++ src/main/java/com/boot/reserveproject/controller/CampController.java
@@ -1,18 +1,20 @@
 package com.boot.reserveproject.controller;
 
+import com.boot.reserveproject.api.CampApiController;
 import com.boot.reserveproject.domain.Camp;
 import com.boot.reserveproject.service.CampService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CampController {
     @Autowired
     private CampService campService;
+    @Autowired
+    private CampApiController campApiController;
 
     @GetMapping("main")
     public String getCamp(Model model) {
@@ -25,8 +27,12 @@ public class CampController {
     public String getDetailsCamp(@RequestParam Long contentId, Model model) {
         Camp camp = campService.getCampById(contentId);
         model.addAttribute("camp", camp);
+
+        //campApiController에 있는 이미지 api
+        String[] campImageList = campApiController.getImageList(contentId);
+        model.addAttribute("campImageList", campImageList);
+
         return "pc/camp/campDetail";
     }
-
 
 }
--- src/test/java/com/boot/reserveproject/service/CampApiServiceTest.java
+++ src/test/java/com/boot/reserveproject/service/CampApiServiceTest.java
@@ -1,14 +1,8 @@
 package com.boot.reserveproject.service;
 
 import com.boot.reserveproject.api.CampApiController;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.HashMap;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CampApiServiceTest {
@@ -19,13 +13,14 @@ class CampApiServiceTest {
     @Autowired
     private CampApiService service;
 
-    @Test
-    public void addData() {
-        int minPageNum = (3176);
-        int maxPageNum = (3436);
-        for (int i = minPageNum; i <= maxPageNum; i++) {
-            service.saveCamps(api.getBasedList(String.valueOf(i)));
-            System.out.println("진행페이지 = " + i + "/" +maxPageNum);
-        }
-    }
+    // camp API DB에 넣을때 사용했음
+//    @Test
+//    public void addData() {
+//        int minPageNum = (3176);
+//        int maxPageNum = (3436);
+//        for (int i = minPageNum; i <= maxPageNum; i++) {
+//            service.saveCamps(api.getBasedList(String.valueOf(i)));
+//            System.out.println("진행페이지 = " + i + "/" +maxPageNum);
+//        }
+//    }
 }
