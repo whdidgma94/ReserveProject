320c500e781b587102eec933525622b66b68dd93
kimyounggyoun <50776744+kimyounggyoun@users.noreply.github.com>
2023-04-11 14:16:41 +0900

    kim

--- src/main/java/com/boot/reserveproject/controller/CampController.java
+++ src/main/java/com/boot/reserveproject/controller/CampController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -21,27 +20,19 @@ public class CampController {
     @Autowired
     private CampApiController campApiController;
 
-//    @GetMapping("/main")
-//    public String getCamp(Model model) {
-//
-//        List<Camp> campList = new ArrayList<>();
-//        for (int i = 1; i <= 5; i++) {
-//            Camp camp = campService.getCampById((long) i);
-//            campList.add(camp);
-//        }
-//        model.addAttribute("campList", campList);
-////
-////        List<String[]> sbrsClList = new ArrayList<>();
-////        for (int i = 0; i < campList.size(); i++) {
-////            String[] sbrsCl = campList.get(i).getSbrsCl().split(",");
-////            sbrsClList.add(sbrsCl);
-////        }
-////        model.addAttribute("sbrsClList", sbrsClList);
-//        return "pc/index";
-//    }
-
     @GetMapping("/main")
     public String getCamp(Model model) {
+        campString(model);
+        return "pc/index";
+    }
+
+    @GetMapping("/camp/campList")
+    public String getCampList(Model model) {
+        campString(model);
+        return "pc/camp/campList";
+    }
+
+    private void campString(Model model) {
         List<Camp> campList = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
             Camp camp = campService.selectOneById((long) i);
@@ -62,12 +53,11 @@ public class CampController {
             if (!campList.get(i).getThemaEnvrnCl().equals("")) {
                 thema = campList.get(i).getThemaEnvrnCl().split(",");
             }
-          themaList.add(Arrays.asList(thema));
+            themaList.add(Arrays.asList(thema));
             System.out.println(Arrays.toString(thema));
         }
         model.addAttribute("sbrsClList", sbrsClList);
         model.addAttribute("themaList", themaList);
-        return "pc/index";
     }
 
     @GetMapping("detailCamp")
