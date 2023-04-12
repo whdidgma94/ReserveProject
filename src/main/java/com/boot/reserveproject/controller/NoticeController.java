292d4edf1c46a26886d90938a87985648c0ba68e
junyoungYoon <junyoungha2@naver.com>
2023-04-12 20:40:25 +0900

    .
    .

--- src/main/java/com/boot/reserveproject/controller/NoticeController.java
+++ src/main/java/com/boot/reserveproject/controller/NoticeController.java
@@ -1,8 +1,8 @@
 package com.boot.reserveproject.controller;
 
 import com.boot.reserveproject.domain.Notice;
-import com.boot.reserveproject.form.MemberForm;
 import com.boot.reserveproject.form.NoticeForm;
+import com.boot.reserveproject.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,10 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class NoticeController {
+
+    private final NoticeService noticeService;
+
     @GetMapping("/admin/notice/new")
     public String noticeForm(Model model) {
         model.addAttribute("noticeForm", new NoticeForm());
@@ -25,6 +29,14 @@ public class NoticeController {
         Notice notice = new Notice();
         notice.setSubject(form.getSubject());
         notice.setContext(form.getSubject());
+        noticeService.createNotice(notice);
         return "admin/index";
+    }
+
+    @PostMapping("/admin/notice/list")
+    public String noticeList(Model model){
+        List<Notice> list = noticeService.getAllNotice();
+        model.addAttribute("noticeList", list);
+        return "admin/notice/noticeList";
     }
 }
--- src/main/java/com/boot/reserveproject/service/NoticeService.java
+++ src/main/java/com/boot/reserveproject/service/NoticeService.java
@@ -4,11 +4,17 @@ import com.boot.reserveproject.domain.Notice;
 import com.boot.reserveproject.repository.NoticeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class NoticeService {
     private NoticeRepository noticeRepository;
 
     public void createNotice(Notice notice){
         noticeRepository.save(notice);
+    }
+
+    public List<Notice> getAllNotice(){
+        return noticeRepository.findAll();
     }
 }
