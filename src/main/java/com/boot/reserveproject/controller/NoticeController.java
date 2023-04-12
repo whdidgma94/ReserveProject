bd6b418ad881b3ba13a04f37786f7967c5f68503
junyoungYoon <junyoungha2@naver.com>
2023-04-12 21:42:24 +0900

    ,
    ,

--- src/main/java/com/boot/reserveproject/controller/NoticeController.java
+++ src/main/java/com/boot/reserveproject/controller/NoticeController.java
@@ -4,11 +4,13 @@ import com.boot.reserveproject.domain.Notice;
 import com.boot.reserveproject.form.NoticeForm;
 import com.boot.reserveproject.service.NoticeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -30,13 +32,28 @@ public class NoticeController {
         notice.setSubject(form.getSubject());
         notice.setContext(form.getSubject());
         noticeService.createNotice(notice);
-        return "admin/index";
+        return "admin/notice/noticeList";
     }
 
-    @PostMapping("/admin/notice/list")
-    public String noticeList(Model model){
+    @GetMapping("/noticeList")
+    public String noticeList(Model model, @RequestParam("type") String type){
         List<Notice> list = noticeService.getAllNotice();
         model.addAttribute("noticeList", list);
+        if(type==null){
+            return null;
+        }
         return "admin/notice/noticeList";
+    }
+
+    @PostMapping("/admin/deleteNotice")
+    public String deleteNotice(@RequestParam("id") Long id){
+        noticeService.deleteNotice(id);
+        return "/admin/notice/noticeList";
+    }
+
+    @GetMapping("/noticeInfo")
+    public ResponseEntity<Notice> getNoticeInfo(@RequestParam("id") Long id) {
+        Notice notice = noticeService.getOneNotice(id);
+        return ResponseEntity.ok(notice);
     }
 }
--- src/main/java/com/boot/reserveproject/service/NoticeService.java
+++ src/main/java/com/boot/reserveproject/service/NoticeService.java
@@ -8,13 +8,28 @@ import java.util.List;
 
 @Service
 public class NoticeService {
-    private NoticeRepository noticeRepository;
+    private final NoticeRepository noticeRepository;
+
+    public NoticeService(NoticeRepository noticeRepository) {
+        this.noticeRepository = noticeRepository;
+    }
 
     public void createNotice(Notice notice){
         noticeRepository.save(notice);
     }
 
     public List<Notice> getAllNotice(){
-        return noticeRepository.findAll();
+        List<Notice> list = noticeRepository.findAll();
+        if (list.isEmpty()) throw new IllegalStateException("데이터가 존재하지않습니다");
+        System.out.println("list = " + list.get(0));
+        return list;
+    }
+
+    public Notice getOneNotice(Long id){
+        return noticeRepository.findById(id).get();
+    }
+
+    public void deleteNotice(Long id){
+        noticeRepository.deleteById(id);
     }
 }
