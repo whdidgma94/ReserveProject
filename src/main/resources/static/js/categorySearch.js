a33d9799d1418090c60040eab855348959f9f48e
younggyun Kim <gyouns76@naver.com>
2023-04-12 18:20:49 +0900

    kim

--- src/main/resources/static/js/categorySearch.js
+++ src/main/resources/static/js/categorySearch.js
@@ -10,36 +10,73 @@ $(function () {
             method: 'GET',
             data: {categories: selectedCategories.join(",")},
             success: function (result) {
-                var response = $.parseJSON(result);
-                var campList = response.campList;
-                if (campList == null || campList.length == 0) {
-                    alert("검색결과가 존재하지 않습니다.");
+                try {
+                    var response = $.parseJSON(result);
+                    var campList = response.campList;
+                    if (campList == null || campList.length == 0) {
+                        alert("검색결과가 존재하지 않습니다.");
+                        return;
+                    }
+                    areaArr = new Array();
+                    for (let i = 0; i < campList.length; i++) {
+                        areaArr.push({
+                            contentId: campList[i].contentId,
+                            firstImageUrl: campList[i].firstImageUrl,
+                            doNm: campList[i].doNm,
+                            sigunguNm: campList[i].sigunguNm,
+                            facltNm: campList[i].facltNm,
+                            lineIntro: campList[i].lineIntro,
 
-                    return;
+                            sbrsCl: campList[i].sbrsCl,
+                            themaEnvrnCl: campList[i].themaEnvrnCl,
+                        });
+                    }
+                    const sbrsClList = sbrsCl.split(",");
+                    let themaList;
+                    if (themaEnvrnCl) {
+                        themaList = themaEnvrnCl.split(",");
+                    } else {
+                        themaList = ["즐거운캠핑"];
+                    }
+
+                    // let campListBoxHtml = "";
+                    // for (let i = 0; i < 5; i++) {
+                    //     campListBoxHtml += '<div><div class="tempCampBox" onClick="details(' + campList[i].contentId + ')">';
+                    //     campListBoxHtml += '<div className="campBoxTop"><div className="campBoxLeft">';
+                    //     campListBoxHtml += '<img style="height: 98%" src="'+campList[i].firstImageUrl+'"/></div>';
+                    //     campListBoxHtml += ' <div className="campBoxRight"><div className="campText">[ <span>'+campList[i].doNm+'</span><span>'+campList[i].sigunguNm+'</span> ]</div>';
+                    //     campListBoxHtml += ;
+                    //     campListBoxHtml += ;
+                    //     campListBoxHtml += ;
+                    //     campListBoxHtml += ;
+                    //     campListBoxHtml += ;
+                    // }
+                    //                 <div className="campText">(주)디노담양힐링파크 지점</div>
+                    //                 <div className="campText">담양 힐링파크에서 일상 속 쌓인 스트레스를 풀어보자</div>
+                    //                 <div className="campSbrsClBox">
+                    //                     <div className="campSbrsCl">
+                    //                         <div className="campSbrsClItem">
+                    //                             <i className="fa-solid fa-bolt"></i>
+                    //                         </div>
+                    //                     </div>
+                    //                 </div>
+                    //             </div>
+                    //         </div>
+                    //     </div>
+                    //     <hr>
+                    // </div>
+
+
+                $('#campListBox').html(campListBoxHtml);
+                } catch (e) {
+                    console.log(e);
+                    alert("오류가 발생했습니다.");
                 }
-                areaArr = new Array();
-                for (let i = 0; i < campList.length; i++) {
-                    areaArr.push({
-                        contentId: campList[i].contentId,
-                        firstImageUrl: campList[i].firstImageUrl,
-                        doNm: campList[i].doNm,
-                        sigunguNm: campList[i].sigunguNm,
-                        facltNm: campList[i].facltNm,
-                        lineIntro: campList[i].lineIntro,
 
-                        lineInt: campList[i].lineIntro,
-                        int: campList[i].intro,
-                        phone: campList[i].tel,
-                        facil: campList[i].sbrsCl,
-                        id: campList[i].contentId
 
-                    });
+                },
+                error: function (xhr, status, error) {
                 }
-
-
-            },
-            error: function (xhr, status, error) {
-            }
-        });
-    });
-});
+                });
+                });
+                });
