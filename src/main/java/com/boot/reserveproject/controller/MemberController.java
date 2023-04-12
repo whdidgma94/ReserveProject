bcda9fdab870969b398bc34ab1fdda3cbb14997b
whdidgma94 <whdidgma94@naver.com>
2023-04-12 15:25:35 +0900

    .

--- src/main/java/com/boot/reserveproject/controller/MemberController.java
+++ src/main/java/com/boot/reserveproject/controller/MemberController.java
@@ -22,15 +22,20 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/pc/member/new")
-    public String joinForm(Model model) {
+    public String joinFormPc(Model model) {
         model.addAttribute("memberForm", new MemberForm());
-
+        System.out.println();
         return "pc/member/memberJoinForm";
     }
+    @GetMapping("/mobile/member/new")
+    public String joinFormMobile(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
 
+        return "mobile/member/memberJoinForm";
+    }
 
     @PostMapping("/pc/member/new")
-    public String joinMember(@Valid MemberForm form, BindingResult result) {
+    public String joinMemberPc(@Valid MemberForm form, BindingResult result) {
 //        if(result.hasErrors()){
 //            return "redirect:/member/new";
 //        }
@@ -56,13 +61,45 @@ public class MemberController {
         memberService.join(member);
         return "pc/index";
     }
+    @PostMapping("/mobile/member/new")
+    public String joinMemberMobile(@Valid MemberForm form, BindingResult result) {
+//        if(result.hasErrors()){
+//            return "redirect:/member/new";
+//        }
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setLoginId(form.getLoginId());
+        member.setPw(form.getPw());
+        System.out.println("domain = " + form.getEmailDomain());
+        member.setEmail(form.getEmailId()+"@"+form.getEmailDomain());
+        member.setPostcode(form.getPostcode());
+        member.setRoadAddress(form.getRoadAddress());
+        member.setJibunAddress(form.getJibunAddress());
+        member.setDetailAddress(form.getDetailAddress());
+        member.setPhone(form.getPhone());
+        System.out.println("form.getPhone() = " + form.getPhone());
+        member.setRegNum1(form.getRegNum1());
+        member.setRegNum2(form.getRegNum2());
+        if(form.getRegNum2().substring(0,1).equals("1")||form.getRegNum2().substring(0,1).equals("3")){
+            member.setGender("male");
+        }else{
+            member.setGender("female");
+        }
+        memberService.join(member);
+        return "mobile/index";
+    }
     @GetMapping("/pc/member/login")
-    public String loginForm(Model model) {
+    public String loginFormPc(Model model) {
         model.addAttribute("loginForm", new LoginForm());
         return "pc/member/memberLoginForm";
     }
+    @GetMapping("/mobile/member/login")
+    public String loginFormMobile(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
+        return "mobile/member/memberLoginForm";
+    }
     @PostMapping("/pc/member/login")
-    private String loginMember(@Valid LoginForm form, BindingResult result, HttpSession session) {
+    private String loginMemberPc(@Valid LoginForm form, BindingResult result, HttpSession session) {
         if (result.hasErrors()) {
             return "redirect:/pc/member/login";
         }
@@ -72,12 +109,29 @@ public class MemberController {
             }
             session.setAttribute("log", form.getLoginId());
         }
-        return "redirect:/main";
+        return "redirect:/pc/main";
     }
-    @GetMapping("/member/logout")
-    private String logout(HttpSession session){
+    @PostMapping("/mobile/member/login")
+    private String loginMemberMobile(@Valid LoginForm form, BindingResult result, HttpSession session) {
+        if (result.hasErrors()) {
+            return "redirect:/mobile/member/login";
+        }
+        if (memberService.checkLogin(form.getLoginId(), form.getPw())) {
+            session.setAttribute("log", form.getLoginId());
+            System.out.println("form.getLoginId() = " + form.getLoginId());
+            System.out.println("form.getPw() = " + form.getPw());
+        }
+        return "redirect:/mobile/index";
+    }
+    @GetMapping("/pc/member/logout")
+    private String logoutPc(HttpSession session){
         session.removeAttribute("log");
-        return "redirect:/main";
+        return "redirect:/pc/main";
+    }
+    @GetMapping("/mobile/member/logout")
+    private String logoutMobile(HttpSession session){
+        session.removeAttribute("log");
+        return "redirect:/mobile/index";
     }
 
     @GetMapping("/pc/members")
@@ -96,9 +150,14 @@ public class MemberController {
         return "false";
     }
     @GetMapping("/pc/member/myPage")
-    public String myPage(HttpSession session, Model model){
+    public String myPagePc(HttpSession session, Model model){
         model.addAttribute("member", memberService.selectMemberById((String) session.getAttribute("log")));
         return "pc/member/memberMyPage";
+    }
+    @GetMapping("/mobile/member/myPage")
+    public String myPageMobile(HttpSession session, Model model){
+        model.addAttribute("member", memberService.selectMemberById((String) session.getAttribute("log")));
+        return "mobile/member/memberMyPage";
     }
 
     @GetMapping("/pc/member/find")
