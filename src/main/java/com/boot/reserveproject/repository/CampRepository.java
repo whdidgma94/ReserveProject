82e1ca935267164ab4f62f88c82d7e4ba4fcff0d
yanhjh <junyoungha2@naver.com>
2023-04-12 17:46:26 +0900

    .

--- src/main/java/com/boot/reserveproject/repository/CampRepository.java
+++ src/main/java/com/boot/reserveproject/repository/CampRepository.java
@@ -1,6 +1,7 @@
 package com.boot.reserveproject.repository;
 
 import com.boot.reserveproject.domain.Camp;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
