91a64d2884a6b16e3ff51dd678930e0e2654cd82
junyoungYoon <junyoungha2@naver.com>
2023-04-13 10:42:44 +0900

    ,
    ,

--- src/main/java/com/boot/reserveproject/controller/QnAController.java
+++ src/main/java/com/boot/reserveproject/controller/QnAController.java
@@ -1,9 +1,26 @@
 package com.boot.reserveproject.controller;
 
+import com.boot.reserveproject.service.QnAService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class QnAController {
+    private final QnAService qnAService;
+
+    @GetMapping("/myQnAList")
+    public String getMyQnAList(String receiver, Model model, @RequestParam("type") String type){
+        model.addAttribute(qnAService.getMyQnA(receiver));
+        if (type.equals("pc")) {
+            return "pc/QnA/QnAList";
+        } else if (type.equals("mobile")) {
+            return "mobile/QnA/QnAList";
+        } else {
+            return "admin/QnA/QnAList";
+        }
+    }
 }
