49eb2d2b94bf7dd9d2abd20b81643289fb1fb15e
whdidgma94 <whdidgma94@naver.com>
2023-04-17 16:01:27 +0900

    .

--- src/main/resources/static/js/showContent.js
+++ src/main/resources/static/js/showContent.js
@@ -1,8 +1,16 @@
-function deleteBoard(boardNo){
+function deleteBoardPc(boardNo){
     var no=boardNo;
 location.href="/../pc/board/deleteBoard?no="+no;
 }
-function updateBoard(boardNo){
+function updateBoardPc(boardNo){
     var no=boardNo;
     location.href="/../pc/board/updateBoard?no="+no;
+}
+function deleteBoardMobile(boardNo){
+    var no=boardNo;
+    location.href="/../mobile/board/deleteBoard?no="+no;
+}
+function updateBoardMobile(boardNo){
+    var no=boardNo;
+    location.href="/../mobile/board/updateBoard?no="+no;
 }
