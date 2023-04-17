9d73d8ff2aaae13a764d7b43a31543977d18e5a9
JunyoungYoon <junyoungha2@naver.com>
2023-04-17 17:32:50 +0900

    ,
    ,

--- src/main/java/com/boot/reserveproject/domain/Camp.java
+++ src/main/java/com/boot/reserveproject/domain/Camp.java
@@ -76,7 +76,7 @@ public class Camp {
     private String themaEnvrnCl; // 테마환경 ( ex.낚시,물놀이 등 )    -------  테마종류
     private String animalCmgCl; // 애완동물 출입 ( ex.가능, 소형견가능 등 )  -------  애완동물
     private String firstImageUrl; // 대표이미지
-
+    private int recommendCnt;
     @OneToOne(fetch = FetchType.LAZY)
     private Reservation reservation;
 
