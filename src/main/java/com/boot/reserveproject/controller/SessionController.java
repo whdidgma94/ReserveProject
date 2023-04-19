636c80e7141937edaa3613ed225cfa4fc900be6b
JunyoungYoon <junyoungha2@naver.com>
2023-04-19 19:45:24 +0900

    .
    .

---
+++ src/main/java/com/boot/reserveproject/controller/SessionController.java
@@ -0,0 +1,29 @@
+package com.boot.reserveproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class SessionController {
+    @GetMapping("/get_session")
+    @ResponseBody
+    public String getSession(HttpSession session) {
+        System.out.println("1");
+        if(session.getAttribute("log")==null){
+            return "false";
+        }else {
+        System.out.println(session.getAttribute("log"));
+            String log = (String) session.getAttribute("log");
+            if(log.equals("admin")){
+                return "true";
+            }else{
+                return "false";
+            }
+        }
+    }
+}
